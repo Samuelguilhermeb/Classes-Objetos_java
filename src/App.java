c20b61a90394e2f1317ffd746cca85bdbafce53d
Samuelguilhermeb <99374269+Samuelguilhermeb@users.noreply.github.com>
2022-04-17 15:55:55 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args){
+
+        Carro carro = new Carro();
+        System.out.println(carro.getPortas());
+        System.out.println(carro.getRodas());
+        System.out.println(carro.getFarol());
+        System.out.println(carro.getRetrovisor());
+        System.out.println(carro.getAssento());
+        System.out.println(carro.getMotor());
+        
+    }
+}
---
+++ src/Carro.java
@@ -0,0 +1,34 @@
+public class Carro {
+
+    private int rodas=4;
+    private int portas=4;
+    private int farol=4;
+    private int retrovisor=3;
+    private int assento=5;
+    private int motor=1;
+
+    public int getRodas(){
+        return  rodas;
+    }
+
+    public int getPortas(){
+        return  portas;
+    } 
+    
+    public int getFarol(){
+        return farol;
+    }
+
+    public int getRetrovisor(){
+        return retrovisor;
+    }
+
+    public int getAssento(){
+        return assento;
+    }
+
+    public int getMotor(){
+        return motor;
+    }
+  
+}
